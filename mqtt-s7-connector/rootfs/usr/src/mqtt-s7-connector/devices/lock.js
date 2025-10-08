6ebb252fbee584839e8b70edd1132518eeac5ffe
RDBTekk <39922529+RDBTekk@users.noreply.github.com>
2025-10-08 19:22:34 +0200

    Simplify repository metadata

---
+++ mqtt-s7-connector/rootfs/usr/src/mqtt-s7-connector/devices/lock.js
@@ -0,0 +1,38 @@
+let device = require('../device.js');
+
+module.exports = class devLock extends device {
+        constructor(plc, mqtt, config) {
+                super(plc, mqtt, config);
+
+                if (config.state) {
+                        this.create_attribute(config.state, 'X', 'state');
+                }
+        }
+
+        send_discover_msg() {
+                const info = {
+                        name: this.name,
+                        payload_lock: 'true',
+                        payload_unlock: 'false',
+                        state_locked: 'true',
+                        state_unlocked: 'false',
+                };
+
+                if (this.attributes['state']) {
+                        if (this.attributes['state'].write_to_s7) {
+                                info.command_topic = this.attributes['state'].full_mqtt_topic + '/set';
+                        }
+                        if (this.attributes['state'].publish_to_mqtt) {
+                                info.state_topic = this.attributes['state'].full_mqtt_topic;
+                        }
+                }
+
+                ['device_class', 'value_template', 'command_template', 'name'].forEach((key) => {
+                        if (this.config[key]) {
+                                info[key] = this.config[key];
+                        }
+                });
+
+                super.send_discover_msg(info);
+        }
+};
