4f8573984c14989f949b9c6cb520debcc101dc6e
RDBTekk <39922529+RDBTekk@users.noreply.github.com>
2025-10-08 16:07:39 +0200

    Normalize ingress URLs for static assets

---
+++ mqtt-s7-connector/rootfs/usr/src/mqtt-s7-connector/devices/light.js
@@ -0,0 +1,67 @@
+let device = require("../device.js");
+
+module.exports = class devLight extends device {
+        constructor(plc, mqtt, config) {
+                super(plc, mqtt, config);
+
+		// add attributes specific for a light
+		// create attribute from config
+
+		// binary state
+		if (config["state"]) {
+			this.create_attribute(config["state"], "X", "state");
+		}
+
+		// one byte for brightness
+		if (config["brightness"]) {
+			this.create_attribute(config["brightness"], "BYTE", "brightness");
+		}
+	}
+
+	send_discover_msg() {
+		let info = {
+			name: this.name,
+			payload_on: "true",
+			payload_off: "false"
+		};
+
+		if (this.attributes["state"]) {
+			// add only command_topic if the attribute is allowed to write
+			if (this.attributes["state"].write_to_s7)
+				info.command_topic = this.attributes["state"].full_mqtt_topic + "/set";
+
+			// add only state_topic if attribute is allowed to read
+			if (this.attributes["state"].publish_to_mqtt)
+				info.state_topic = this.attributes["state"].full_mqtt_topic;
+		}
+
+		if (this.attributes["brightness"]) {
+			// add only brightness_command_topic if the attribute is allowed to write
+			if (this.attributes["brightness"].write_to_s7)
+				info.brightness_command_topic = this.attributes["brightness"].full_mqtt_topic + "/set";
+
+			// add only brightness_state_topic if attribute is allowed to read
+			if (this.attributes["brightness"].publish_to_mqtt)
+				info.brightness_state_topic = this.attributes["brightness"].full_mqtt_topic;
+		}
+
+		// Support home-assistant discover options
+		// https://www.home-assistant.io/integrations/mqtt/
+		[
+			'device_class',
+			'value_template',
+			'command_template',
+			'brightness_value_template',
+			'brightness_command_template',
+			'name',
+		].forEach((key) => {
+			if(this.config[key]) {
+				info[key] = this.config[key];
+			}
+		});
+
+		super.send_discover_msg(info);
+	}
+
+
+}
